f03c4e314e57f439eecc72e9d7fe856344dcc384
dqn <dqn270@gmail.com>
2023-03-29 22:58:22 +0900

    chore: update

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -2,13 +2,6 @@ import { ImageResponse } from "@vercel/og";
 
 export const runtime = "experimental-edge";
 
-// const encodedPath = [
-//   46, 46, 47, 46, 46, 47, 46, 46, 47, 97, 115, 115, 101, 116, 115, 47, 102, 111,
-//   110, 116, 46, 119, 111, 102, 102,
-// ];
-
-// const path = encodedPath.map((n) => String.fromCharCode(n)).join("");
-// const font = fetch(new URL(path, import.meta.url)).then((res) =>
 const font = fetch("https://dqn-og.vercel.app/font.woff").then((res) =>
   res.arrayBuffer(),
 );
@@ -42,7 +35,6 @@ export async function GET(req: Request): Promise<ImageResponse> {
             alignItems: "center",
             justifyContent: "center",
             fontFamily: "NotoSansJP-Bold",
-            // fontFamily: "sans-serif",
             fontWeight: "bold",
             color: "#383635",
             whiteSpace: "pre-wrap",
